d2d0bca0986e83d16d279b04e11d5003776da94a
dongly <zldongly@outlook.com>
2023-07-30 18:59:48 +0800

    'feat:timer'

--- timer.go
+++ timer.go
@@ -1,19 +1,27 @@
 package timer
 
 import (
+	"errors"
+	"math"
 	"sync"
 	"time"
 )
 
+var (
+	ErrNotExist = errors.New("timer: not exist task id")
+)
+
 const (
-	_defaultSize = 32
+	_defaultSize               = 32
+	_maxDuration time.Duration = math.MaxInt64 // 1<<63 - 1
 )
 
 func New(opts ...Option) *Timer {
 	tr := &Timer{
-		size:  _defaultSize,
-		tasks: &chain{},
-		free:  &stack{},
+		signal: time.NewTimer(_maxDuration),
+		size:   _defaultSize,
+		tasks:  &chain{},
+		free:   &stack{},
 	}
 
 	for _, o := range opts {
@@ -38,10 +46,28 @@ type Timer struct {
 // public
 
 // Add 添加任务
+func (tr *Timer) Add(expire time.Time, fn func()) int64 {
+	tr.mu.Lock()
+	id := tr.add(expire, fn)
+	tr.mu.Unlock()
+
+	return id
+}
 
 // Set 重置任务时间
+func (tr *Timer) Set(id int64, expire time.Time) error {
+	tr.mu.Lock()
+	err := tr.set(id, expire)
+	tr.mu.Unlock()
+	return err
+}
 
 // Del 按id删除任务
+func (tr *Timer) Del(id int64) {
+	tr.mu.Lock()
+	tr.del(id)
+	tr.mu.Unlock()
+}
 
 // private
 
@@ -49,6 +75,7 @@ type Timer struct {
 // 初始化
 func (tr *Timer) init() {
 	tr.grow()
+	go tr.run()
 }
 
 // grow 扩容
@@ -59,6 +86,17 @@ func (tr *Timer) grow() {
 		t := &(tasks[i])
 		tr.putFree(t)
 	}
+}
+
+// reload 重新计时
+func (tr *Timer) reload() {
+	if tr.tasks.head == nil {
+		tr.signal.Reset(_maxDuration)
+		return
+	}
+
+	d := time.Until(tr.tasks.head.expire)
+	tr.signal.Reset(d)
 }
 
 // getFree 取一个空闲任务
@@ -76,6 +114,84 @@ func (tr *Timer) putFree(t *Task) {
 		return
 	}
 	tr.free.push(t)
+}
+
+// del 按id删除任务
+func (tr *Timer) del(id int64) {
+	var t *Task
+	t = tr.tasks.del(id)
+	if t == nil {
+		return
+	}
+
+	tr.putFree(t)
+	tr.reload()
+}
+
+// set 修改时间
+func (tr *Timer) set(id int64, expire time.Time) error {
+	var t *Task
+	t = tr.tasks.del(id)
+	if t == nil {
+		return ErrNotExist
+	}
+
+	t.expire = expire
+	tr.tasks.insert(t)
+	tr.reload()
+	return nil
+}
+
+// add 添加时间
+func (tr *Timer) add(expire time.Time, fn func()) int64 {
+	tr.pk = tr.pk + 1
+	t := tr.getFree()
+
+	t.id = tr.pk
+	t.expire = expire
+	t.fn = fn
+
+	tr.tasks.insert(t)
+	tr.reload()
+
+	return t.id
+}
+
+// exec
+func (tr *Timer) exec() {
+	tr.mu.Lock()
+
+	t := tr.tasks.head
+
+	if t == nil { // 任务链表为空
+		tr.mu.Unlock()
+		return
+	}
+
+	if time.Now().Before(t.expire) { // 首个任务时间还没到
+		tr.reload()
+		tr.mu.Unlock()
+		return
+	}
+
+	t = tr.tasks.pop() // 取出任务
+	fn := t.fn
+	tr.putFree(t) // 放回空闲链表
+
+	tr.reload()
+	tr.mu.Unlock()
+
+	if fn != nil {
+		fn()
+	}
+}
+
+// run
+func (tr *Timer) run() {
+	for {
+		<-tr.signal.C
+		tr.exec()
+	}
 }
 
 // Option
--- timer_test.go
+++ timer_test.go
@@ -1,1 +1,42 @@
-package timer
+package timer_test
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/zldongly/timer"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestTimer(t *testing.T) {
+	var (
+		tr    = timer.New(timer.Size(8))
+		wants = []int{2, 3, 4, 6, 7, 9, 5}
+		list  = make([]int, 0, 10)
+		wg    sync.WaitGroup
+	)
+
+	wg.Add(7)
+	for i := 0; i < 10; i++ {
+		i := i
+		e := time.Now().Add(time.Millisecond * time.Duration(i))
+		tr.Add(e, func() {
+			list = append(list, i+1)
+			wg.Done()
+		})
+	}
+
+	tr.Del(1)
+	tr.Del(10)
+	tr.Del(8)
+	// 重置时间 不存在的任务报错
+	err := tr.Set(8, time.Now().Add(time.Second))
+	assert.Equal(t, timer.ErrNotExist, err)
+	err = tr.Set(5, time.Now().Add(time.Millisecond*20))
+	assert.Equal(t, error(nil), err)
+
+	wg.Wait()
+	for i := range wants {
+		assert.Equal(t, wants[i], list[i])
+	}
+}
